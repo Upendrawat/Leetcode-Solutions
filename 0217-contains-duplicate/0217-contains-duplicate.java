56d5b34b56850dc3e88c291be3203284c60b9c34
Upendra Rawat <rawatupendra689@gmail.com>
2026-08-23 16:33:45 +0530

    Time: 24 ms (28.61%), Space: 81.3 MB (95.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,29 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
-        // In hashmap it is majorly used for frequensy storing index or value with index for this we can use hashset that is mainly used for the unique element or the duplicate elements
-
-
-        // HashMap<Integer,Integer> hp = new HashMap<>();
-
-        // for(int i=0; i<nums.length; i++){
-        //     hp.put(nums[i] , hp.getOrDefault(nums[i],0)+1);
-        // }
-
-        // for(Integer num : hp.values()){
-        //     if(num > 1){
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        // METHOD 2 using hashset
-        HashSet<Integer> hs = new HashSet<>();
-        for(int num : nums){
-            if(hs.contains(num)){
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i-1]){
                 return true;
             }
-            hs.add(num);
         }
         return false;
     }
